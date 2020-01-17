9a0c45ee7a4e1f1c93bd31db8117bbb7d657f21e
Michael J. Harms <harmsm@gmail.com>
2020-01-16 20:26:46 -0800

    switch over to likelihood package for fitting

--- pytc/__init__.py
+++ pytc/__init__.py
@@ -10,12 +10,14 @@ __all__ = ["experiments","indiv_models","global_connectors","util"]
 from . import experiments
 from . import indiv_models
 from . import global_connectors
-from . import fitters
 from . import util
 
-# Load these defaults (which will be used for vast majority of experiments) 
+# Load these defaults (which will be used for vast majority of experiments)
 # into the global namespace for convenience
 from .experiments import ITCExperiment
 from .global_fit import GlobalFit
 from .global_connectors import GlobalConnector
 
+# use the likelihood package to bring in fitting tools.  To keep the api
+# consistent, call it fitters.
+import likelihood as fitters
